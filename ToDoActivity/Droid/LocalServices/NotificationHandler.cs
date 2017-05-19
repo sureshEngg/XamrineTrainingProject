7e961c41c27cd9ab2fc2dc2ef18ceb8dfbfdc055
sohitEngg <sharma.sohit645@gmail.com>
2017-05-19 18:28:26 +0530

    Fixed issues related to Notification and GPS location.

--- ToDoActivity/Droid/LocalServices/NotificationHandler.cs
+++ ToDoActivity/Droid/LocalServices/NotificationHandler.cs
@@ -5,45 +5,50 @@ using Xamarin.Forms;
 
 namespace ToDoActivity.Droid
 {
-	public class NotificationHandler
-	{
+    public class NotificationHandler
+    {
 
-		private static NotificationHandler _instance = new NotificationHandler();
+        private static NotificationHandler _instance = new NotificationHandler();
 
-		static public NotificationHandler SharedInstance()
-		{
-			return _instance;
-		}
+        static public NotificationHandler SharedInstance()
+        {
+            return _instance;
+        }
 
-		public void ScheduleNotification(ActivityModel activityModel)
-		{
-			if (activityModel != null)
-			{
-				Intent alarmIntent = new Intent(Forms.Context, typeof(NotificationPublisher));
-				alarmIntent.PutExtra("title", activityModel.Name);
-				alarmIntent.PutExtra("message", activityModel.Description);
+        // Schedule the notification for the future time when a TODO due date appears
+        public void ScheduleNotification(ActivityModel activityModel)
+        {
+            if (activityModel != null)
+            {
+                Intent alarmIntent = new Intent(Forms.Context, typeof(NotificationPublisher));
+                alarmIntent.PutExtra("title", activityModel.Name);
+                alarmIntent.PutExtra("message", activityModel.Description);
 
-				PendingIntent pendingIntent = PendingIntent.GetBroadcast(Forms.Context, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
-				AlarmManager alarmManager = (AlarmManager)Forms.Context.GetSystemService(Context.AlarmService);
+                PendingIntent pendingIntent = PendingIntent.GetBroadcast(Forms.Context, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
+                AlarmManager alarmManager = (AlarmManager)Forms.Context.GetSystemService(Context.AlarmService);
 
-				// Set fire date
-				alarmManager.Set(AlarmType.RtcWakeup, activityModel.DueDate.Millisecond, pendingIntent);
-			}
-		}
+                double FireTime = activityModel.DueDate.ToUniversalTime().Subtract(
+                    new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                        ).TotalMilliseconds;
 
-		public void UpdateNotification(ActivityModel activityModel)
-		{
-			CancelNotification(activityModel);
-			ScheduleNotification(activityModel);
-		}
+                // Set fire date
+                alarmManager.Set(AlarmType.RtcWakeup, (long)FireTime, pendingIntent);
+            }
+        }
 
-		public void CancelNotification(ActivityModel activityModel)
-		{
-			//Intent intent = new Intent(Forms.Context, typeof(NotificationPublisher));
-			//PendingIntent sender = PendingIntent.GetBroadcast(Forms.Context, 0, intent, 0);
+        public void UpdateNotification(ActivityModel activityModel)
+        {
+            CancelNotification(activityModel);
+            ScheduleNotification(activityModel);
+        }
 
-			//AlarmManager alarmManager = (AlarmManager)Forms.Context.GetSystemService(Context.AlarmService);
-			//alarmManager.Cancel(sender);
-		}
-	}
+        public void CancelNotification(ActivityModel activityModel)
+        {
+            //Intent intent = new Intent(Forms.Context, typeof(NotificationPublisher));
+            //PendingIntent sender = PendingIntent.GetBroadcast(Forms.Context, 0, intent, 0);
+
+            //AlarmManager alarmManager = (AlarmManager)Forms.Context.GetSystemService(Context.AlarmService);
+            //alarmManager.Cancel(sender);
+        }
+    }
 }
--- ToDoActivity/Droid/MainActivity.cs
+++ ToDoActivity/Droid/MainActivity.cs
@@ -1,28 +1,36 @@
 ﻿using System;
 
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
-using ToDoActivity.Droid;
 using Android.Locations;
 using System.Collections.Generic;
 using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Android;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Android.Content;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace ToDoActivity.Droid
 {
     [Activity(Label = "ToDoActivity.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : FormsAppCompatActivity, ILocationListener
+    public class MainActivity : FormsAppCompatActivity
     {
         // All variable declared here
         static readonly string TAG = "X:" + typeof(MainActivity).Name;
-        LocationManager _locationManager;
         string _locationProvider;
+        int WIFI_SETTINGS_REQ_CODE = 100;
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionsLocation =
+            {
+                Manifest.Permission.AccessCoarseLocation,
+                Manifest.Permission.AccessFineLocation
+            };
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -32,49 +40,131 @@ namespace ToDoActivity.Droid
             base.OnCreate(savedInstanceState);
             Forms.Init(this, savedInstanceState);
 
-            //To initialize GPS service for the App
-            InitializeLocationManager();
-
+            CheckForGPSSettings();
             LoadApplication(new App());
         }
 
-        void InitializeLocationManager()
+        // To check if Wifi is available from any mode either from GPS or Wifi
+        private void CheckForGPSSettings()
         {
-            _locationManager = (LocationManager)GetSystemService(LocationService);
-            Criteria criteriaForLocationService = new Criteria
-            {
-                Accuracy = Accuracy.Fine
-            };
-            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+            Boolean gps_enabled = false;
+            Boolean network_enabled = false;
 
-            if (acceptableLocationProviders.Any())
+            LocationManager _locationManager = (LocationManager)GetSystemService(LocationService);
+            try
             {
-                _locationProvider = acceptableLocationProviders.First();
+                gps_enabled = _locationManager.IsProviderEnabled(LocationManager.GpsProvider);
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, "Error in GPS = " + ex.Message, ToastLength.Long).Show();
+            }
+
+            try
+            {
+                network_enabled = _locationManager.IsProviderEnabled(LocationManager.NetworkProvider);
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, "Error in Network = " + ex.Message, ToastLength.Long).Show();
+            }
+
+            if (!gps_enabled)
+            {
+                var intent = new Intent(Android.Provider.Settings.ActionLocationSourceSettings);
+                StartActivityForResult(intent, WIFI_SETTINGS_REQ_CODE);
             }
             else
             {
-                _locationProvider = string.Empty;
+                SetUpLocationServicesAsync();
             }
-            Location loc = _locationManager.GetLastKnownLocation(_locationProvider);
-            LocationHandler.Instance().CurrentLocation = loc;
         }
 
-        protected override void OnResume()
+        //When User come back from the GPS Setting screen, need to initiate GPS stuff
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
-            base.OnResume();
-            _locationManager.RequestLocationUpdates(_locationProvider, 1000, 0, this);
+            if (requestCode == WIFI_SETTINGS_REQ_CODE)
+                CheckForGPSSettings();
         }
 
-        protected override void OnPause()
+        // Prior to fetching current location
+        private async void SetUpLocationServicesAsync()
         {
-            base.OnPause();
-            _locationManager.RemoveUpdates(this);
+            await TryGetLocationAsync();
         }
 
-        //Geo Location is finding here
-        public void OnLocationChanged(Location location)
+        // Checking for devive version as marshmallow and above device require permission to be granted
+        async Task TryGetLocationAsync()
         {
-            LocationHandler.Instance().CurrentLocation = location;
+            if ((int)Build.VERSION.SdkInt < 23)
+            {
+                await GetLocationAsync();
+                return;
+            }
+
+            await GetLocationPermissionAsync();
+        }
+
+        // For marshmallow and above devices It is required to ask for the GPS permission. allow or deny are the options.
+        async Task GetLocationPermissionAsync()
+        {
+            const string permission = Manifest.Permission.AccessFineLocation;
+
+            if (CheckSelfPermission(permission) == (int)Permission.Granted)
+            {
+                await GetLocationAsync();
+                return;
+            }
+
+            if (ShouldShowRequestPermissionRationale(permission))
+            {
+                //Explain to the user why we need to read the contacts
+                Toast.MakeText(this, "Location access is required to read current location.", ToastLength.Long).Show();
+                RequestPermissions(PermissionsLocation, RequestLocationId);
+                return;
+            }
+
+            RequestPermissions(PermissionsLocation, RequestLocationId);
+        }
+
+        // Once permission access or denied this would be invoked to proceed with the application
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            switch (requestCode)
+            {
+                case RequestLocationId:
+                    {
+                        if (grantResults[0] == (int)Permission.Granted)
+                        {
+                            GetLocationAsync();
+                        }
+                        else
+                        {
+                            Toast.MakeText(this, "GPS will not be available for Application due denial of GPS permission!", ToastLength.Long).Show();
+                        }
+                    }
+                    break;
+            }
+        }
+
+        // Finding the current GPS location
+        async Task GetLocationAsync()
+        {
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 500000;
+                var position = await locator.GetPositionAsync(50000);
+                Location loc = new Location(_locationProvider);
+                loc.Latitude = position.Latitude;
+                loc.Longitude = position.Longitude;
+                LocationHandler.Instance().CurrentLocation = loc;
+            }
+            catch (Exception ex)
+            {
+
+                Toast.MakeText(this, "Unable to get location: " + ex.ToString(), ToastLength.Long).Show();
+            }
         }
 
         public void OnProviderDisabled(string provider) { }
